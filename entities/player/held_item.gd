e482038833d97b408eb6c7e26fb5691fd2045282
scalkin <nicklastandfest@gmail.com>
2022-10-28 10:25:53 -0500

    Made a system for weapons having different stats. TAB switches weapons in debug builds

--- entities/player/held_item.gd
+++ entities/player/held_item.gd
@@ -18,34 +18,48 @@ var item_position = UP
 var item_applied_position = 0
 var attack_positions = []
 var attack_trail_generated
+var item_sprite_list = [
+	load("res://game/items/weapons/dagger_1.png"),
+	load("res://game/items/weapons/dagger_2.png")
+]
+var item_swing_size_list = [120, 130]
+var item_held_distance_list = [13, 14]
+var item_attack_speed_list = [500, 400]
 
 export var swing_size = 120
 export var item_held_distance = 13
-export var rotation_speed = 200
+export var attack_speed = 500
+var item_sprite = 0
 
 var trail_effect = preload("res://game/effects/attack_trail.tscn")
 
 func attack(delta):
-	var applied_rotation_speed = ((sin((3.14*rotated)/swing_size)*2000)+0.001)*delta
+	var applied_rotation_speed = ((sin((3.14*rotated)/swing_size)*1500)+(attack_speed*0.001))*delta
 	match item_position:
 		DOWN:
 			rotate(-deg2rad(applied_rotation_speed))
 			rotated += applied_rotation_speed
-			if rotated >= swing_size/2 and not attack_trail_generated:
+			if rotated >= swing_size:
+				$Sprite.hide()
+				rotate(deg2rad(rotated/2))
 				calculate_trail_points()
 				attack_trail()
-			if rotated >= swing_size:
 				emit_signal("attack_finished")
 				flip_item_position()
+				idle()
+				$Sprite.show()
 		UP:
 			rotate(deg2rad(applied_rotation_speed))
 			rotated += applied_rotation_speed
-			if rotated >= swing_size/2 and not attack_trail_generated:
+			if rotated >= swing_size:
+				$Sprite.hide()
+				rotate(-deg2rad(rotated/2))
 				calculate_trail_points()
 				attack_trail()
-			if rotated >= swing_size:
 				emit_signal("attack_finished")
 				flip_item_position()
+				idle()
+				$Sprite.show()
 
 func calculate_trail_points():
 	#TODO add 2 more points in between middle and ends
@@ -56,7 +70,7 @@ func calculate_trail_points():
 		Vector2(
 			sin(deg2rad(swing_size/4))*item_held_distance,
 			-cos(deg2rad(swing_size/4))*item_held_distance),
-		Vector2(0, -12.5),
+		Vector2(0, -(item_held_distance - 0.5)),
 		Vector2(
 			-sin(deg2rad(swing_size/4))*item_held_distance,
 			-cos(deg2rad(swing_size/4))*item_held_distance),
@@ -100,3 +114,12 @@ func flip_item_position():
 			item_position = DOWN
 		DOWN:
 			item_position = UP
+
+func update_item():
+	$Sprite.texture = item_sprite_list[global.held_item_id]
+	swing_size = item_swing_size_list[global.held_item_id]
+	item_held_distance = item_held_distance_list[global.held_item_id]
+	attack_speed = item_attack_speed_list[global.held_item_id]
+
+func _physics_process(_delta):
+	update_item()
--- game/global.gd
+++ game/global.gd
@@ -1,9 +1,37 @@
-extends Node
+extends Node2D
+
+enum{
+	MOUSE
+	CONTROLLER
+}
 
 var player_accel = 500
 var player_max_speed = 200
-var held_item = 0
-var item_textures = [
-	"res://game/items/weapons/dagger_1.png", "res://game/items/weapons/dagger_2.png"
-]
+export var held_item_id = 0
 var inventory = [1, 0]
+var target_mode = MOUSE
+var last_mouse_pos = Vector2.ZERO
+
+func _process(_delta):
+	var controller_input_vector = Vector2.ZERO
+	controller_input_vector.x = Input.get_axis("target_left", "target_right")
+	controller_input_vector.y = Input.get_axis("target_up", "target_down")
+	controller_input_vector = controller_input_vector.normalized()
+	if Input.is_action_just_pressed("ui_focus_next") and OS.is_debug_build():
+		match held_item_id:
+			0:
+				held_item_id = 1
+			1:
+				held_item_id = 0
+	if controller_input_vector != Vector2.ZERO:
+		target_mode = CONTROLLER
+	elif get_local_mouse_position() != last_mouse_pos:
+		last_mouse_pos = get_local_mouse_position()
+		target_mode = MOUSE
+	match target_mode:
+		MOUSE:
+			pass
+		CONTROLLER:
+			Input.warp_mouse_position((get_viewport().size/2)+(controller_input_vector*250))
+			
+
